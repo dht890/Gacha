fc3abb764f3a61af784a748970efd5207367043b
David Tran <david.tran890@gmail.com>
2025-06-20 21:18:40 -0700

    cleanup

--- backend/models/profiles.py
+++ backend/models/profiles.py
@@ -1,13 +1,11 @@
 class Profile:
-    def __init__(self, id, username, xp_lvl=1, pfp=None):
-        self.id = id  # This will be unique
+    def __init__(self, username, xp_lvl=1, pfp=None):
         self.username = username  # This will also be unique
         self.xp_lvl = xp_lvl
         self.pfp = pfp
 
     def to_dict(self):
         return {
-            "id": self.id,
             "username": self.username,
             "xp_lvl": self.xp_lvl,
             "pfp": self.pfp,
@@ -16,7 +14,6 @@ class Profile:
     @classmethod
     def from_dict(cls, data):
         return cls(
-            id=data["id"],
             username=data["username"],
             xp_lvl=data.get("xp_lvl", 1),
             pfp=data.get("pfp")
@@ -25,12 +22,9 @@ class Profile:
     @staticmethod
     def create_indexes(db):
         """Create unique indexes for the profile collection"""
-        db.get_collection('profiles').create_index('id', unique=True)
         db.get_collection('profiles').create_index('username', unique=True) 
 
     def validate(self):
-        if not isinstance(self.id, str) or not self.id:
-            raise ValueError("Profile ID must be a non-empty string.")
         
         if not isinstance(self.username, str) or not self.username:
             raise ValueError("Username must be a non-empty string.")
